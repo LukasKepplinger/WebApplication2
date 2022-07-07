33601659ddff7e947bd1bbdfc3d1fef11a47b8eb
LukasKepplinger <20162010@atn.ac.at>
2022-07-07 10:34:10 +0200

    a

--- WebApplication2/Models/Person.cs
+++ WebApplication2/Models/Person.cs
@@ -4,6 +4,8 @@ namespace WebApplication2
 {
     public class Person
     {
+        public int Id { get; set; }
+
         [Required]
         public string FirstName { get; set; }
         [Required]
---
+++ WebApplication2/Pages/RealDbPage.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Data;
+
+namespace WebApplication2.Pages
+{
+    public class RealDbPageModel : PageModel
+    {
+        [BindProperty]
+        public Person Person { get; set; }
+        [BindProperty]
+        public List<Person> Persons { get; set; }
+
+        public RealDbPageModel(MyDbContext context)
+        {
+            Context = context;
+        }
+
+        public MyDbContext Context { get; }
+
+        public void OnGet()
+        {
+            Persons = Context.Persons.ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                Context.Persons.Add(Person);
+                Context.SaveChanges();
+            }
+            return RedirectToAction("RealDbPage");
+        }
+    }
+}
