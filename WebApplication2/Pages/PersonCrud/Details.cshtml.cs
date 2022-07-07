7e06f26e754e796368343c49fd2925e9c0d40e86
LukasKepplinger <20162010@atn.ac.at>
2022-07-07 10:52:48 +0200

    a

---
+++ WebApplication2/Pages/PersonCrud/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2;
+using WebApplication2.Data;
+
+namespace WebApplication2.Pages.PersonCrud
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication2.Data.MyDbContext _context;
+
+        public DetailsModel(WebApplication2.Data.MyDbContext context)
+        {
+            _context = context;
+        }
+
+      public Person Person { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Persons == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FirstOrDefaultAsync(m => m.Id == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Person = person;
+            }
+            return Page();
+        }
+    }
+}
